93eaa89ee8002fa002e09bcb93c50c620feadf46
Hugo ST <hugost03@gmail.com>
2024-11-09 12:55:51 +0100

    Update libreria.c

--- libreria.c
+++ libreria.c
@@ -9,6 +9,11 @@ int head (int N){
   char linea[1024];
   
   while ((i<N) && (fgets(linea, 1024, stdin) != NULL)) {
+    //Comprobar si se alcanza el final del fichero antes de N lineas
+    if(feof(stdin) && i < N){
+      fprintf(stderr, "La entrada no tiene %d lineas. No son suficientes", N);
+      return 1;
+    }
     fprintf(stdout, "%s", linea);
     i++;
   }
@@ -16,10 +21,17 @@ int head (int N){
 }
 
 int tail (int N){
+  //Variables
   int i = 0;
   char linea[1024];
-  
+  //Array que almacena las lineas
   char **arr = (char**)malloc(N * sizeof(char *));
+  if (arr == NULL) {
+    fprintf(stderr, "Error al asignar memoria.\n");
+    return 1;
+  }
+  
+  //Bucle que almacena las lineas
   while (fgets(linea, 1024, stdin) != NULL){
     arr[i] = strdup(linea);
     if (arr[i] == NULL){
@@ -32,13 +44,17 @@ int tail (int N){
     }
     i = (i + 1) % N;
   }
-  for (int k = 0; k<N; k++){
-    printf("%s", arr[k]);  
+  //Imprimir por salida estandar
+  for (int k = i; k<N + i; k++){
+    printf("%s", arr[k % N]);  
   }  
+ 
+  //Liberar memoria
   for (int m=0;m<N; m++){
     free(arr[m]);
   } 
   free(arr);
+  
   return 0;
 }
 int longlines (int N){
