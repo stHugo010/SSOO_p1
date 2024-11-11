f4166dc517c87444a7a2adbd24e21e00ab58b570
Hugo ST <hugost03@gmail.com>
2024-11-11 13:22:40 +0100

    Update compila.sh

--- compila.sh
+++ compila.sh
@@ -6,3 +6,5 @@ ar -rv liblibreria.a libreria.o
 
 #Generar ejecutable de test.c enlazado con la libreria
 gcc -o test test.c -L. -llibreria
+
+echo "Formato de uso: ./test [-head, -tail, -longLines] [N]"
