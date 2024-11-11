d8959a7927f040964c1f805b7553bb88c5202164
Hugo ST <hugost03@gmail.com>
2024-11-11 13:18:28 +0100

    Update test.c

--- test.c
+++ test.c
@@ -1,24 +1,35 @@
 //implementar librerias
+#include "libreria.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "libreria.h"
+
 
 //funcion principal
 int main(int argc, char *argv[]){
   int numLineas = 10;         //numero de lineas por defecto              
   char *funcion = argv[1];    //variable que guarda la funcion 
-  //comprobar si se ha pasado un numero de lineas
-  if(argc = 2 && argv[2]>0){
+  
+  //comprobar que el numero de argumentos es valido
+  if(argc < 1 || argc > 3){
+    printf("Numero de argumentos invalido.");
+    return 1;
+  }
+  
+  //comprobar si se ha pasado un numero de lineas valido 
+  if(argv[2]>0){
     numLineas = atoi(argv[2]);
   }
   //llamadas a las funciones correspondientes
   if (strcmp(funcion, "-head") == 0){
     head(numLineas);
+    return 0;
   } else if (strcmp(funcion, "-tail") == 0){
     tail(numLineas);
+    return 0;
   } else if (strcmp(funcion, "-longLines") == 0){
     longlines(numLineas);
+    return 0;
   } else{
     printf("La función introducida no es correcta");
     return 1;
